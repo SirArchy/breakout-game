ca609bdbcf9a276005340667ecd97c09810efb97
SirArchy <wnbemail123@googlemail.com>
2024-03-20 17:01:14 +0100

    changed level difficulty

--- src/game/levels.js
+++ src/game/levels.js
@@ -10,25 +10,25 @@ const getBlocks = (rows, columns) =>
 export const LEVELS = [
   {
     lives: 5,
-    paddleWidth: 3.5,
+    paddleWidth: 2.5,
     speed: 1,
     blocks: getBlocks(3, 6)
   },
   {
     lives: 4,
-    paddleWidth: 3,
+    paddleWidth: 2,
     speed: 1.4,
     blocks: getBlocks(4, 7)
   },
   {
     lives: 3,
-    paddleWidth: 2.5,
+    paddleWidth: 1.5,
     speed: 1.8,
     blocks: getBlocks(5, 8)
   },
   {
-    lives: 3,
-    paddleWidth: 2,
+    lives: 2,
+    paddleWidth: 1,
     speed: 2.2,
     blocks: getBlocks(6, 9)
   },
